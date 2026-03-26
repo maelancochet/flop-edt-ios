bfffaf8816bc06615cb9d2824b272581c21b49d2
Ma3lich <ma3lich@MacBook-Air-de-Ma3lich.local>
2026-03-26 15:17:17 +0100

    ajoute de tout le projet

---
+++ flopEDT/flopEDT/flopEDTApp.swift
@@ -0,0 +1,17 @@
+//
+//  flopEDTApp.swift
+//  flopEDT
+//
+//  Created by Ma3lich on 26/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct flopEDTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
